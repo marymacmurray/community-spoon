620eb8e59648626379f5fe842088ba1df04debe2
marymacmurray <marymac@maryxmac.com>
2020-02-03 20:41:12 -0500

    updated all html, css, image, js files

--- js/main.js
+++ js/main.js
@@ -1,44 +1,65 @@
 // NO API required for Axios calls.
-const BASE_URL = 'https://randomuser.me/api/';
+const BASE_URL = 'https://randomuser.me/api/?results=5000&?inc=name,location,picture';
 const dropdown = document.querySelector('#userselect');
-const tryMebutton = document.querySelector('#try-me');
+const userSelectbutton = document.querySelector('#user-select-button');
 const userDisplay = document.querySelector('#user-info');
-const cuisineDropdown = document.querySelector('#cuisine-button');
+const cuisineDropdown = document.querySelector('#cuisine');
+const cuisineSelectbutton = document.querySelector('#cuisine-select-button')
 
+const MEALSDB_URL = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
-let users = async () => {
-  await axios.get(`${BASE_URL}?seed`)
-    .then(res => {  //this res is a variable.  it could be pineapple. 
-      //.then causes the axios call object returned to be passed to res.  
-      //.then is SAME AS: remove all of .then and .catch but you wouldn't see what was happening and get your error back unless you declare the variable:{const myCategories = await axios.get ...,{"x-api-key":API_KEY}})
+let meals = async () => {
+  await axios.get(`${MEALSDB_URL}`)
+    .then(res => {
       console.log(res);
-      let userDiv = res.data.results[0].picture.large;
-      let userLocatP = res.data.results[0].location.country;
-      let user
-      userDisplay.innerHTML = `<img src=${userDiv} id="userImg"><p>${userLocatP}</p>`;
-      const userCountry = res.data.results[0].location.country;
-      // for (let i = 0; i < userCountry.length; i++) {
-      //   dropdown.innerHTML += `<option id=${userCountry[i].}> ${userinfo[i].name.}</option>`
-      // }
-    }
-    )
+      const cuisine = res.data.meals[0].strArea;
+      console.log(cuisine)
+      for (let i = 0; i < cuisine.length; i++) {
+        dropdown.innerHTML += `<option id=${cuisine[i]}>${cuisine[i]}</option>`
+      }
+    })
     .catch(error => {
       console.log(error);
     })
 }
+meals();
 
-tryMebutton.addEventListener('click', async () => {
-  const categoryId = dropdown[dropdown.selectedIndex].id;
-  try {
-    const response = await axios.get(`https://api.thecatapi.com/v1/images/search?category_ids=${categoryId}`,//this is when you go to the API docs and get the image id info.  everything after the 'search?' is the query parameter.
-    )
-    // console.log(response);
-    const catPhoto = response.data[0].url; //dig into the doc to find what the name is.
-    picDisplay.innerHTML = `<img src=${catPhoto}>`
-  } catch (error) {
-    console.log(error);
-  }
+cuisineSelectbutton.addEventListener('click', async () => {
+  const cuisineCategory = cuisineDropdown.options[cuisineDropdown.selectedIndex].value;
+
 })
+
+let users = async () => {
+  await axios.get(`${BASE_URL}`)
+    .then(res => {  //this res is a variable.  it could be pineapple. 
+      //.then causes the axios call object returned to be passed to res.  
+      console.log(res);
+
+      // let userDiv = res.data.results[0].picture.large;
+      let userUS = res.data.results
+      // console.log(userUS)
+      let userNY = userUS.filter(user => {
+        return user.location.state === "New York";
+      })
+      console.log(userNY)
+    })
+    .catch(error => {
+      console.log(error);
+    })
+}
+users();
+  // cusineInput.addEventListener('click', async () => {
+  //   const categoryId = cuisineInput.options[cuisineInput.selectedIndex].value;
+  //   try {
+  //     const response = await axios.get(`https://randomuser.me/api/?seed=${categoryId}`,//this is when you go to the API docs and get the image id info.  everything after the 'search?' is the query parameter.
+  //     )
+  //     // console.log(response);
+  //     //     const catPhoto = response.data[0].url; //dig into the doc to find what the name is.
+  //     //     picDisplay.innerHTML = `<img src=${catPhoto}>`
+  //     //   } catch (error) {
+  //     //     console.log(error);
+  //     //   }
+  // })
 
 // window.addEventListener('load', async () => {
 //   const categoryId = dropdown[dropdown.selectedIndex].id;
@@ -52,5 +73,3 @@ tryMebutton.addEventListener('click', async () => {
 //     console.log(error);
 //   }
 // })
-
-users();
